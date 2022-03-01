3f9c478ee4f41d00f84c6f7da32a93685a06ffe3
umiz <940803428@qq.com>
2022-03-01 22:34:24 +0800

    version 1.2

--- app/src/main/java/com/example/kosong/Main2Activity.java
+++ app/src/main/java/com/example/kosong/Main2Activity.java
@@ -3,23 +3,19 @@ package com.example.kosong;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 
-
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     private TextView textView; //用于显示时间的view
-    private String failureTime = "2022-12-23 23:00:00"; // 失效时间
+    private String failureTime = "2022-12-24 16:19:45"; // 失效时间
 
     @SuppressLint("HandlerLeak")
     private Handler handler = new Handler() {
@@ -35,45 +31,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView1);
-        handler.sendMessage(handler.obtainMessage());
+        setContentView(R.layout.activity_main2);
+        textView = findViewById(R.id.text);
+        handler.sendMessage(handler.obtainMessage()); // 在这里开始 时间计时
     }
 
-    /** Called when the user taps the Send button */
-    public void changeVocabulary(View view) {
-        Intent intent = new Intent(this, VocabularyActivity.class);
-        startActivity(intent);
-    }
-    /* 测试用 */
-    public void test(View view){
-        Intent intent = new Intent(this, Main2Activity.class);
-        startActivity(intent);
-    }
-
-
-
-
-
-    /* 实现日历倒计时 */
     @SuppressLint("SetTextI18n")
     private void setTime() {
         @SuppressLint("SimpleDateFormat")
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
             long nowTime = System.currentTimeMillis(); //获取当前时间
             Date overTime = simpleDateFormat.parse(failureTime); //利用SimpleDateFormat来把字符串日期转换为Date对象类型
             long a = overTime.getTime() - nowTime; // 计算总的时间差（毫秒级别）
             if (a > 0) { //如果大于零 说明时间有剩余
+
                 // 总时间（a）
                 // 总天数( day*(1000 * 60 * 60 * 24) )
                 // 总小时数（ hour * (1000 * 60 * 60) ）
                 // 总分钟数（ minute * (1000 * 60) ）
+
+                // 总时间 中有几天
                 long day = a / (1000 * 60 * 60 * 24);
+                // 总时间 去除天数后，还剩多少小时
                 long hour = (a - day * (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
+                // 总时间 去除天数与小时后，还剩多少分钟
                 long minute = (a - day * (1000 * 60 * 60 * 24) - hour * (1000 * 60 * 60)) / (1000 * 60);
+                // 总时间 去除天数小时与分钟后，还剩多少秒
                 long second = (a - day * (1000 * 60 * 60 * 24) - hour * (1000 * 60 * 60) - minute * (1000 * 60)) / 1000;
-                textView.setText(day + " Days\n" + hour + " Hrs " + minute + " Mins " + second + " s");
+                //刷新ui显示
+                textView.setText(day + "Days " + hour + "Hrs " + minute + "Mins " + second + "s");
             }else {
                 Toast.makeText(this, "小于等于零", Toast.LENGTH_SHORT).show();
             }
@@ -81,5 +68,4 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-
 }
